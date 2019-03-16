37e9c1bbbf393b106c5ecb8b9d4c4883567fe5aa
Aaron Casanova <aaronccasanova@gmail.com>
2019-03-16 15:55:50 -0700

    fix: pre-commit typo and remove json from eslint check

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   linters: {
-    '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
+    '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|vue)': [
       'eslint . --fix',
+    ],
+    '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
       'prettier --write',
       // Note: add this to indivual package's lint-staged config
       // 'jest --findRelatedTests',
