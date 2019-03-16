830ee216609749cde8cb658a209122e4325f3990
Aaron Casanova <aaronccasanova@gmail.com>
2019-03-16 16:17:51 -0700

    feat: replace ghooks with husky

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 const eslintrc = require('./.eslintrc')
 
 module.exports = eslintrc
-
-const testEslint = 'testing eslint'
-
-console.log(
-  'testing                                                                ',
-)
-
-console.log(testEslint)
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   linters: {
     '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|vue)': [
-      // Temp. removed json from glob
-      // '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
       'eslint --fix',
       'prettier --write',
       // Note: add this to indivual package's lint-staged config
       // 'jest --findRelatedTests',
       'git add',
     ],
+    // TODO: Look into running the linter globs synchronously
+    // the first without JSON for eslint, and prettier / git add for the other
+    // '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': []
   },
 }
