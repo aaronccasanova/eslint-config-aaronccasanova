64d9a0d54e6b7e4ff706ae6fec09e67f3eec373e
Aaron Casanova <aaronccasanova@gmail.com>
2019-03-16 15:58:25 -0700

    testing husky pre-commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const eslintrc = require('./.eslintrc')
 
 module.exports = eslintrc
+
+let testEslint = 'testing eslint'
+
+console.log(
+  'testing                                                                ',
+)
+
+console.log(testEslint)
